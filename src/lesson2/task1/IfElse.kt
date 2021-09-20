51655fffdbd97463fe6ed5c30389e77da5388783
Ivan <ivan24891@mail.ru>
2021-09-20 18:45:39 +0300

    Сделал полностью 2 урок

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -3,6 +3,10 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
+import ru.spbstu.kotlin.generate.assume.retry
+import ru.spbstu.wheels.product
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.sqrt
 
@@ -68,7 +72,12 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    if (age % 100 in 5..20) return ("$age лет")
+    else if (age % 10 in 2..4) return ("$age года")
+    else if (age % 10 == 1) return ("$age год")
+    else return ("$age лет")
+}
 
 /**
  * Простая (2 балла)
@@ -81,7 +90,13 @@ fun timeForHalfWay(
     t1: Double, v1: Double,
     t2: Double, v2: Double,
     t3: Double, v3: Double
-): Double = TODO()
+): Double {
+    val halfWay: Double = (t1 * v1 + t2 * v2 + t3 * v3) / 2
+    if (t1 * v1 >= halfWay) return halfWay / v1
+    return if (t1 * v2 < halfWay && t1 * v1 + t2 * v2 >= halfWay) t1 + (halfWay - v1 * t1) / v2
+    else t1 + t2 + (halfWay - t1 * v1 - t2 * v2) / v3
+}
+
 
 /**
  * Простая (2 балла)
@@ -96,7 +111,12 @@ fun whichRookThreatens(
     kingX: Int, kingY: Int,
     rookX1: Int, rookY1: Int,
     rookX2: Int, rookY2: Int
-): Int = TODO()
+): Int {
+    if (kingX != rookX2 && kingY != rookY2 && kingX != rookX1 && kingY != rookY1) return 0
+    if ((kingX == rookX1 || kingY == rookY1) && kingX != rookX2 && kingY != rookY2) return 1
+    return if ((kingX == rookX2 || kingY == rookY2) && kingX != rookX1 && kingY != rookY1) 2
+    else 3
+}
 
 /**
  * Простая (2 балла)
@@ -112,7 +132,13 @@ fun rookOrBishopThreatens(
     kingX: Int, kingY: Int,
     rookX: Int, rookY: Int,
     bishopX: Int, bishopY: Int
-): Int = TODO()
+): Int {
+    if (kingX != rookX && kingY != rookY && (abs(kingX - bishopX) != abs(kingY - bishopY))) return 0
+    if ((kingX == rookX || kingY == rookY) && (abs(kingX - bishopX) != abs(kingY - bishopY))) return 1
+    return if (kingX != rookX && kingY != rookY && (abs(kingX - bishopX) == abs(kingY - bishopY))) 2
+    else 3
+}
+
 
 /**
  * Простая (2 балла)
@@ -122,7 +148,14 @@ fun rookOrBishopThreatens(
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    val sortedTriangles = mutableListOf(a, b, c)
+    sortedTriangles.sort()
+    if (sortedTriangles[2] >= sortedTriangles[1] + sortedTriangles[0]) return -1
+    if (sqr(sortedTriangles[2]) < (sqr(sortedTriangles[1]) + sqr(sortedTriangles[0]))) return 0
+    return if (sqr(sortedTriangles[2]) == (sqr(sortedTriangles[1]) + sqr(sortedTriangles[0]))) 1
+    else 2
+}
 
 /**
  * Средняя (3 балла)
@@ -132,4 +165,10 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
+    if (b - c >= 0 && c > a && b < d) return b - c
+    if (d - a >= 0 && c < a && b > d) return d - a
+    if (c >= a && b >= d) return d - c
+    return if (c <= a && b <= d) b - a
+    else -1
+}
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,8 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import lesson7.task1.alignFileByWidth
+import kotlin.math.abs
 
 /**
  * Пример
@@ -18,7 +20,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean = number / 1000 + number / 100 % 10 == number / 10 % 10 + number % 10
 
 /**
  * Простая (2 балла)
@@ -27,7 +29,8 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+    abs(x2 - x1) == abs(y2 - y1) || x2 == x1 || y2 == y1
 
 
 /**
@@ -36,7 +39,12 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    if (month == 4 || month == 6 || month == 9 || month == 11) return 30
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) return 31
+    return if ((month == 2) && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) 29
+    else return 28
+}
 
 /**
  * Простая (2 балла)
@@ -59,4 +67,6 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
+    a * b <= r * s || a * c <= r * s || b * c <= r * s
+

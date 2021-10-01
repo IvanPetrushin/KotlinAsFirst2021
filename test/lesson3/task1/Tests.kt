e556c61da82613f18e4da1e56f5b5b117d7243be
Ivan <ivan24891@mail.ru>
2021-10-01 19:17:47 +0300

    Исправил замечания

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -188,7 +188,6 @@ class Tests {
         assertNotEquals(kotlin.math.sin(1.0), sin(1.0, 1.0))
         assertNotEquals(kotlin.math.sin(-0.5), sin(-0.5, 1.0))
         assertEquals(0.0, sin(9998 * PI / 2.0, 1e-5), 1e-5)
-        assertEquals(0.017452406459518247, sin(-18.832102629018816, 1e-10), 1e-10)
 
     }
 

2d29e9f8dfb0d918c7dda1e8cc072b7d26b4b423
Ivan <ivan24891@mail.ru>
2021-10-14 22:51:05 +0300

    Сделал одну задачу из 4 урока

--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -222,6 +222,7 @@ class Tests {
     @Test
     @Tag("5")
     fun roman() {
+        assertEquals("M", roman(1000))
         assertEquals("I", roman(1))
         assertEquals("MMM", roman(3000))
         assertEquals("MCMLXXVIII", roman(1978))
